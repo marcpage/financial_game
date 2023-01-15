231a4cda55a29e46cb43644f8d407be61d033c27
Marc Page <MarcAllenPage@gmail.com>
2023-01-15 11:22:05 -0600

    Moving session keys to their own file

---
+++ tests/test_sessionkey.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+
+import time
+import hashlib
+import types
+
+import financial_game.sessionkey
+
+
+def test_basic():
+    while time.localtime().tm_min == 59:
+        time.sleep(0.100)  # To prevent test flakiness around hour changes
+
+    secret = "Setec astronomy"
+    user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3"
+    user = types.SimpleNamespace(id=5, password_hash=hashlib.sha256("password".encode()).hexdigest())
+    headers = {
+        'User-Agent': user_agent,
+    }
+
+    key = financial_game.sessionkey.create(user, headers, secret)
+    user_id, password_hash = financial_game.sessionkey.parse(key, headers, secret)
+    assert user_id == user.id, f"user.id: {user.id} user_id: {user_id}"
+    assert password_hash == user.password_hash, f"user.password_hash: {user.password_hash} password_hash: {password_hash}"
+
+
+def test_old():
+    while time.localtime().tm_min == 59:
+        time.sleep(0.100)  # To prevent test flakiness around hour changes
+
+    secret = "Setec astronomy"
+    user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3"
+    user = types.SimpleNamespace(id=5, password_hash=hashlib.sha256("password".encode()).hexdigest())
+    headers = {
+        'User-Agent': user_agent,
+    }
+
+    key = financial_game.sessionkey.create(user, headers, secret, hour_delta=-1)
+    user_id, password_hash = financial_game.sessionkey.parse(key, headers, secret)
+    assert user_id == user.id, f"user.id: {user.id} user_id: {user_id}"
+    assert password_hash == user.password_hash, f"user.password_hash: {user.password_hash} password_hash: {password_hash}"
+
+
+def test_bad():
+    while time.localtime().tm_min == 59:
+        time.sleep(0.100)  # To prevent test flakiness around hour changes
+
+    secret = "Setec astronomy"
+    user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3"
+    user = types.SimpleNamespace(id=5, password_hash=hashlib.sha256("password".encode()).hexdigest())
+    headers = {
+        'User-Agent': user_agent,
+    }
+
+    key = financial_game.sessionkey.create(user, headers, secret, hour_delta=+1)
+    user_id, password_hash = financial_game.sessionkey.parse(key, headers, secret)
+    assert user_id is None, f"user_id: {user_id}"
+    assert password_hash is None, f"password_hash: {password_hash}"
+
+
+if __name__ == "__main__":
+    test_basic()
+    test_old()
+    test_bad()
