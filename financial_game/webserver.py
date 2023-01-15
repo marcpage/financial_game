a21edaeba6dffdadc2d2b81793415c934df742bb
Marc Page <MarcAllenPage@gmail.com>
2023-01-15 10:09:56 -0600

    Adding session cookie

--- financial_game/webserver.py
+++ financial_game/webserver.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-""" scheduling restaurant staff
+""" Root webserver
 """
 
 
@@ -8,6 +8,11 @@ import flask
 
 import financial_game.template
 import financial_game.model
+import financial_game.sessionkey
+
+
+# TODO: Get session key secret from settings file  # pylint: disable=fixme
+SECRET = "secret"
 
 
 def create_app(database):
@@ -20,13 +25,25 @@ def create_app(database):
     def home(message=None):
         """default location for the server, home"""
 
-        if "user-id" not in flask.request.cookies:
+        if "user-id" in flask.request.cookies:
+            user_id, password_hash = financial_game.sessionkey.parse(
+                flask.request.cookies["user-id"], flask.request.headers, SECRET
+            )
+            user = database.get_user(user_id)
+
+            if user is not None and user.password_hash != password_hash:
+                user = None
+
+        else:
+            user = None
+
+        if user is None:
             contents = financial_game.template.render(
                 "templates/home.html.mako", message=message
             )
 
         else:
-            contents = f"<html><body>Welcome {flask.request.cookies.get('user-id')}</body></html>"
+            contents = f"<html><body>Welcome {user.name}</body></html>"
 
         return contents, 200
 
@@ -39,11 +56,13 @@ def create_app(database):
     @app.route("/login", methods=["POST"])
     def login():
         user = database.find_user(flask.request.form["email"])
-        print(f"user = {user}")
 
         if user and user.password_matches(flask.request.form["password"]):
             response = flask.make_response(flask.redirect(flask.url_for("home")))
-            response.set_cookie("user-id", f"{user.id}")
+            session_key = financial_game.sessionkey.create(
+                user, flask.request.headers, SECRET
+            )
+            response.set_cookie("user-id", session_key)
 
         else:
             response = flask.make_response(
@@ -57,7 +76,6 @@ def create_app(database):
     @app.errorhandler(404)
     def page_not_found(error=None):
         """Returns error page 404"""
-        print(f"error = '{error}'")
         return f"<html><body>404 not found<p>{error}</p></body></html>", 404
 
     return app
