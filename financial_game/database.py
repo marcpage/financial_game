f43ab06887fc82102d40e2d8095a1f791e2a89cd
Marc Page <MarcAllenPage@gmail.com>
2023-01-23 15:28:57 -0600

    Adding all testing and docs for database

--- financial_game/database.py
+++ financial_game/database.py
@@ -110,7 +110,12 @@ class Threadsafe(threading.Thread):
 
 
 class Connection:
+    """A database connection"""
+
     def __init__(self, database):
+        """Create the connection
+        database - an instance of Sqlite3 or equivalent
+        """
         self.__db = database
 
     @staticmethod
@@ -124,21 +129,33 @@ class Connection:
     def execute(
         self, sql_command: str, replacements: any, commit: bool = True
     ) -> (int, [str], list, int):
+        """execute SQL command w/ the given replacements and optionally committing"""
         return self.__db.execute(sql_command, replacements, commit=commit)
 
-    def fetch_one_or_none(self, sql_command: str, **replacements) -> any:
-        as_object = replacements.get("as_object", True)
-        results = self.__db.execute(sql_command, replacements, fetch_all=False)
+    def fetch_one_or_none(self, _sql_command_: str, **_replacements_) -> any:
+        """Return the first match or None if no matches
+        _as_object_ - (True) If True return objects, False return dictionaries
+        """
+        as_object = _replacements_.get("_as_object_", True)
+        results = self.__db.execute(_sql_command_, _replacements_, fetch_all=False)
         return Connection.__convert(results[1], results[2], as_object)
 
-    def fetch_all(self, sql_command: str, **replacements) -> [any]:
-        as_objects = replacements.get("as_objects", True)
-        results = self.__db.execute(sql_command, replacements, fetch_all=True)
+    def fetch_all(self, _sql_command_: str, **_replacements_) -> [any]:
+        """Return all results from the query
+        _as_object_ - (True) If True return objects, False return dictionaries
+        """
+        as_objects = _replacements_.get("_as_object_", True)
+        results = self.__db.execute(_sql_command_, _replacements_, fetch_all=True)
         return [Connection.__convert(results[1], r, as_objects) for r in results[2]]
 
-    def create_table(self, _table_name_: str, **description):
-        if_not_exists = description.get("if_not_exists", True)
-        description_string = ", ".join(f'"{n}" {v}' for n, v in description.items())
+    def create_table(self, _table_name_: str, **_description_):
+        """Create a table
+        _table_name_ - the name of the table to create
+        _description_ - pass in name of column mapped to SQL description
+        _if_not_exists_ - (True) If True add the "IF NOT EXISTS" clause
+        """
+        if_not_exists = _description_.get("_if_not_exists_", True)
+        description_string = ", ".join(f'"{n}" {v}' for n, v in _description_.items())
         exists_string = " IF NOT EXISTS" if if_not_exists else ""
         self.execute(
             f"""CREATE TABLE{exists_string} "{_table_name_}" ({description_string});""",
@@ -146,13 +163,18 @@ class Connection:
             commit=False,
         )
 
-    def insert(self, _table_name_: str, **data) -> any:
-        as_object = data.get("as_object", True)
-        commit = data.get("commit", True)
-        row_name = data.get("row_name", "id")
-        columns = sorted(data)
-        placeholders = ", ".join("?" for _ in range(0, len(data)))
-        replacements = [data[c] for c in columns]
+    def insert(self, _table_name_: str, **_data_) -> any:
+        """Insert a new row in the table
+        _table_name_ - the table to insert data into
+        _data_ - map of column name to data to put in the table
+        _as_object_ - (True) If True return an object, False return a dictionary
+        """
+        as_object = _data_.get("_as_object_", True)
+        commit = _data_.get("commit", True)
+        row_name = _data_.get("row_name", "id")
+        columns = sorted(_data_)
+        placeholders = ", ".join("?" for _ in range(0, len(_data_)))
+        replacements = [_data_[c] for c in columns]
         column_string = ", ".join(f'"{c}"' for c in columns)
         results = self.execute(
             f"""INSERT INTO {_table_name_} ({column_string}) VALUES({placeholders});""",
@@ -162,45 +184,65 @@ class Connection:
         assert results[3] > 0, "No rows were inserted {results}"
 
         if row_name is not None:
-            data = dict(data)
-            data[row_name] = results[0]
+            _data_ = dict(_data_)
+            _data_[row_name] = results[0]
 
-        return types.SimpleNamespace(**data) if as_object else data
+        return types.SimpleNamespace(**_data_) if as_object else _data_
 
-    def get_one_or_none(self, _table_name_: str, *columns, **replacements) -> any:
-        as_object = replacements.get("as_object", True)
-        where = replacements.get("where", None)
+    def get_one_or_none(self, _table_name_: str, *_columns_, **_replacements_) -> any:
+        """Get the first result or None if there are no results
+        _table_name_ - The table to query
+        _columns_ - list the names of any columns you want returned or none for all columns
+        _replacements_ - :name in where will be replaced with name=value
+        _as_object_ - (True) If True return an object, False return a dictionary
+        """
+        as_object = _replacements_.get("_as_object_", True)
+        where = _replacements_.get("where", None)
         column_string = (
-            "*" if len(columns) == 0 else ", ".join(f'"{c}"' for c in columns)
+            "*" if len(_columns_) == 0 else ", ".join(f'"{c}"' for c in _columns_)
         )
         where_string = "" if where is None else f" WHERE {where}"
         return self.fetch_one_or_none(
             f"""SELECT {column_string} FROM {_table_name_}{where_string};""",
-            **replacements,
+            **_replacements_,
             as_object=as_object,
         )
 
-    def get_all(self, _table_name_: str, *columns, **replacements) -> [any]:
-        as_objects = replacements.get("as_objects", True)
-        where = replacements.get("where", None)
+    def get_all(self, _table_name_: str, *_columns_, **_replacements_) -> [any]:
+        """Get all the results that match
+        _table_name_ - The table to query
+        _columns_ - list the names of any columns you want returned or none for all columns
+        _replacements_ - :name in where will be replaced with name=value
+        _as_objects_ - (True) If True return objects, False return dictionaries
+        """
+        as_objects = _replacements_.get("_as_objects_", True)
+        where = _replacements_.get("where", None)
         column_string = (
-            "*" if len(columns) == 0 else ", ".join(f'"{c}"' for c in columns)
+            "*" if len(_columns_) == 0 else ", ".join(f'"{c}"' for c in _columns_)
         )
         where_string = "" if where is None else f" WHERE {where}"
         return self.fetch_all(
             f"""SELECT {column_string} FROM {_table_name_}{where_string};""",
-            **replacements,
+            **_replacements_,
             as_objects=as_objects,
         )
         # group_by:list=None, order_clause=None
         # order_ascending=True, limit=None, offset=None
 
-    def delete(self, _table_name_: str, where: str, **replacements) -> int:
-        commit = replacements.get("commit", True)
+    def delete(self, _table_name_: str, _where_: str, **_replacements_) -> int:
+        """delete a row from the table
+        _table_name_ - the table to delete the row from
+        _where_ - The where clause
+        _replacements_ - :name in where will be replaced with name=value
+        """
+        commit = _replacements_.get("commit", True)
         results = self.execute(
-            f"""DELETE FROM {_table_name_} WHERE {where}""", replacements, commit=commit
+            f"""DELETE FROM {_table_name_} WHERE {_where_}""",
+            _replacements_,
+            commit=commit,
         )
         return results[3]
 
     def close(self):
+        """Close out the database"""
         self.__db.close()
--- tests/test_database.py
+++ tests/test_database.py
@@ -3,9 +3,70 @@
 
 import tempfile
 import os
+import threading
+import queue
 
 import financial_game.database
 from financial_game.database import Sqlite3,Threadsafe
+
+RECORDS_TO_CREATE = 1000
+THREADS_TO_TEST = 50
+
+def add_items(db, table, in_queue, out_queue):
+    while True:
+        try:
+            work = in_queue.get(timeout=0.100)
+            db.insert(table, **work)
+
+        except queue.Empty:
+            break
+
+    out_queue.put(db.get_all(table))
+
+
+def test_Threadsafe():
+    with tempfile.TemporaryDirectory() as workspace:
+        db_path = os.path.join(workspace, "test.sqlite3")
+        db = financial_game.database.Connection(Threadsafe(db_path, Sqlite3))
+        db.create_table("user",
+            id="INTEGER PRIMARY KEY",
+            name="VARCHAR(50)",
+            email="VARCHAR(50)",
+            password_hash="VARCHAR(64)",
+            sponsor_id="INTEGER")
+        user_queue = queue.Queue()
+        users_queue = queue.Queue()
+        threads = [threading.Thread(target=add_items,
+                                    args=(db, 'user', user_queue, users_queue),
+                                    daemon=True)
+                    for _ in range(0, THREADS_TO_TEST)]
+
+        for thread in threads:
+            thread.start()
+
+        for i in range(0, RECORDS_TO_CREATE):
+            user_queue.put({
+                'name': f"user #{i}",
+                'email': f"user{i}@company.org",
+                'password_hash': "3bce676cf7e5489dd539b077eb38888a1c9d42b23f88bc5c1f2af863f14ab23c"})
+
+        for thread in threads:
+            thread.join()
+
+        iterations = 0
+
+        while True:
+            try:
+                results = users_queue.get(timeout=0.100)
+                assert len(results) == RECORDS_TO_CREATE
+                assert len(set(u.email for u in results)) == RECORDS_TO_CREATE
+                iterations += 1
+
+            except queue.Empty:
+                break
+
+        assert iterations == THREADS_TO_TEST
+        db.close()
 
 
 def test_Sqlite3():
@@ -77,3 +138,4 @@ def test_Sqlite3():
 
 if __name__ == "__main__":
     test_Sqlite3()
+    test_Threadsafe()
